c0480f76b9e51918526b9514c35e74c3e5c2c452
Christopher Basketfield <chrisBB@ak.net>
2019-01-30 12:22:19 -0900

    added models for home image and vimeo links

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Home_Image, Vimeo_Link
+
+admin.site.register(Home_Image)
+admin.site.register(Vimeo_Link)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Home_Image(models.Model):
+    image = models.ImageField(upload_to="photos/", blank=True, null=True)
+    description = models.CharField(max_length=140, blank=True, null=True)
+
+
+class Vimeo_Link(models.Model):
+    video = models.URLField()
