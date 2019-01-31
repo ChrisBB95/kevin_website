4135c0a822317db6a3258f117a73886e23f6f5f9
Christopher Basketfield <chrisBB@ak.net>
2019-01-31 14:12:02 -0900

    more homepage styling, vimeo embedding

--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Home_Image
+from .models import Home_Image, Vimeo_Link
 
 
 def home_view(request):
     context = {
-        'home_image': Home_Image.objects.first().image.url
-
+        'home_image': Home_Image.objects.first().image.url,
+        'vimeo_link': Vimeo_Link.objects.first().video
     }
     return render(request, 'home.html', context)
