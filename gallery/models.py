00ba3572f931b7e0e2f7aea9a14fdfd373623a43
Christopher Basketfield <chrisBB@ak.net>
2019-02-07 16:19:57 -0900

    fixed file deletion on model edit

--- gallery/models.py
+++ gallery/models.py
@@ -17,32 +17,24 @@ class Gallery_Image(models.Model):
 
 
 @receiver(models.signals.post_delete, sender=Gallery_Image)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    """
-    Deletes file from filesystem
-    when corresponding `Gallery_Image` object is deleted.
-    """
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+def submission_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
 
 
 @receiver(models.signals.pre_save, sender=Gallery_Image)
 def auto_delete_file_on_change(sender, instance, **kwargs):
-    """
-    Deletes old file from filesystem
-    when corresponding `Gallery_Image` object is updated
-    with new file.
-    """
     if not instance.pk:
         return False
 
     try:
         old_file = sender.objects.get(pk=instance.pk).image
-    except Gallery_Image.DoesNotExist:
+    except sender.DoesNotExist:
         return False
 
     new_file = instance.image
     if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+        try:
+            if os.path.isfile(old_file.path):
+                os.remove(old_file.path)
+        except ValueError:
+            return False
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
@@ -17,32 +18,24 @@ class Product(models.Model):
 
 
 @receiver(models.signals.post_delete, sender=Product)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    """
-    Deletes file from filesystem
-    when corresponding `Gallery_Image` object is deleted.
-    """
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+def submission_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
 
 
 @receiver(models.signals.pre_save, sender=Product)
 def auto_delete_file_on_change(sender, instance, **kwargs):
-    """
-    Deletes old file from filesystem
-    when corresponding `Home_Image` object is updated
-    with new file.
-    """
     if not instance.pk:
         return False
 
     try:
         old_file = sender.objects.get(pk=instance.pk).image
-    except Home_Image.DoesNotExist:
+    except sender.DoesNotExist:
         return False
 
     new_file = instance.image
     if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+        try:
+            if os.path.isfile(old_file.path):
+                os.remove(old_file.path)
+        except ValueError:
+            return False
